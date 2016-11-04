631243b15328b32977a44c179856cfa37ac9b0df
jyotiduhan <duhanjyoti@gmail.com>
2016-11-04 14:23:59 +0530

    fixed some oops moment

--- app/javascripts/containers/index.js
+++ app/javascripts/containers/index.js
@@ -1,4 +1,3 @@
 export MainContainer from './MainContainer/MainContainer'
 export HomeContainer from './HomeContainer/HomeContainer'
 export HeaderContainer from './HeaderContainer/HeaderContainer'
-export SelectBoxContainer from './SelectBoxContainer/SelectBoxContainer'
